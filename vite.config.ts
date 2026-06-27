78723b511daebff9155fccb98e93412cf3dbb28b
Eflin7240 <syariflol213@gmail.com>
2026-06-27 14:20:38 +0800

    Uji coba perbaikan gambar

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/CV.-Bisnis-Pro-Komputama/',
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
