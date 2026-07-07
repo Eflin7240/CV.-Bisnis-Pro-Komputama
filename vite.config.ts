1ac16b1ca47f877ba0801a349404848f05661beb
Eflin7240 <syariflol213@gmail.com>
2026-07-07 08:42:12 +0800

    Benerin bug vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/CV.-Bisnis-Pro-Komputama/',
   plugins: [
     react(),
     tailwindcss(),
